bc1e07389853c8438d65cb05bb11359434247e0b
simrandhankar <simran.dhankar@itmtb.com>
2023-03-07 23:14:42 +0530

    completed task

--- official-codechef/src/pages/index.jsx
+++ official-codechef/src/pages/index.jsx
@@ -1,12 +1,15 @@
 import LeadershipTeam from "@/data/leadershipTeam";
 import { Inter } from "next/font/google";
-
+import Depart_Lead from "../components/Home/Depart_Lead"
+import ContactUs from "../components/Home/ContactUs";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <h1 className={inter.className}>Home Page</h1>
+      {/* <h1 className={inter.className}>Home Page</h1> */}
+      <Depart_Lead/>
+      <ContactUs/>
     </main>
   );
 }
