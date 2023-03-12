d118b7dcea7ae7ecabb550b6e151827d374e1bf3
simrandhankar <simran.dhankar@itmtb.com>
2023-03-12 19:50:15 +0530

    font change

--- official-codechef/src/pages/index.jsx
+++ official-codechef/src/pages/index.jsx
@@ -1,12 +1,13 @@
 import LeadershipTeam from "@/data/leadershipTeam";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import Depart_Lead from "../components/Home/Depart_Lead"
 import ContactUs from "../components/Home/ContactUs";
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({ subsets: ["latin"],
+weight: '400' });
 
 export default function Home() {
   return (
-    <main>
+    <main className={inter.className}>
       {/* <h1 className={inter.className}>Home Page</h1> */}
       <Depart_Lead/>
       <ContactUs/>
